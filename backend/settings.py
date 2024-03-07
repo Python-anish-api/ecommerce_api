458236cc2d1f88ff438a10bbfe6c71c219ae56c7
Anish Gharti <anishchengre@kali.chengre>
2024-03-07 21:36:34 +0545

    drf started

--- backend/settings.py
+++ backend/settings.py
@@ -31,12 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+        # 'drf_yasg',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'customer',
+    'vendor',
+    'store',
+    'userauths',
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist'
 ]
 
 MIDDLEWARE = [
@@ -112,10 +121,18 @@ USE_I18N = True
 USE_TZ = True
 
 
+AUTH_USER_MODEL = 'userauths.User'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- userauths/models.py
+++ userauths/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from django.db.models.signals import post_save
+from django.utils.html import mark_safe
+from shortuuid.django_fields import ShortUUIDField
+import os
 
-# Create your models here.
+GENDER = (
+    ("female", "Female"),
+    ("male", "Male"),
+)
+
+
+    
+
+    
+class User(AbstractUser):
+    username = models.CharField(max_length=500, null=True, blank=True)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=500, null=True, blank=True)
+    phone = models.CharField(max_length=500)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
+    def __unicode__(self):
+        return self.username
+
+    def save(self, *args, **kwargs):
+        email_username = self.email.split('@')
+        if self.full_name == "" or self.full_name == None:
+             self.full_name = self.email
+             self.username = email_username
+        super(User, self).save(*args, **kwargs)
+    
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='avatars/', default='avatars/default-user.jpg', null=True, blank=True)
+    full_name = models.CharField(max_length=1000, null=True, blank=True)
+    about = models.TextField( null=True, blank=True)
+    
+    gender = models.CharField(max_length=500, choices=GENDER, null=True, blank=True)
+    country = models.CharField(max_length=1000, null=True, blank=True)
+    city = models.CharField(max_length=500, null=True, blank=True)
+    state = models.CharField(max_length=500, null=True, blank=True)
+    # newsletter = models.BooleanField(default=False)
+    address = models.CharField(max_length=1000, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    pid = ShortUUIDField(unique=True, length=10, max_length=20, alphabet="abcdefghijklmnopqrstuvxyz") 
+    # wishlist = models.ManyToManyField("store.Product", blank=True)
+    # type = models.CharField(max_length=500, choices=GENDER, null=True, blank=True)
+
+
+    class Meta:
+        ordering = ["-date"]
+
+    def __str__(self):
+        if self.full_name:
+            return str(self.full_name)
+        else:
+            return str(self.user.full_name)
+    
+    def save(self, *args, **kwargs):
+        if self.full_name == "" or self.full_name == None:
+             self.full_name = self.user.full_name
+        
+        super(Profile, self).save(*args, **kwargs)
+
+
+    # def thumbnail(self):
+    #     return mark_safe('<img src="/media/%s" width="50" height="50" object-fit:"cover" style="border-radius: 30px; object-fit: cover;" />' % (self.image))
+    
+    
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
